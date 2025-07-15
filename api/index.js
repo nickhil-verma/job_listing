148def5e7aaeccfe7f140c628b5364c8c5714e69
Nikhil Verma <vermanick75@gmail.com>
2025-07-16 00:09:34 +0530

    Added cors

--- api/index.js
+++ api/index.js
@@ -64,3 +64,7 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'Internal server error' });
   }
 }
+
+export default function handler(req, res) {
+  res.status(200).json({ message: "🌍 Hello from Vercel root route!" });
+}
