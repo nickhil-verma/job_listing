5a2a85cc3bdfc61699eb48a96ff38d38b499b063
Nikhil Verma <vermanick75@gmail.com>
2025-07-21 19:52:41 +0530

    increased limit of post

--- api/index.js
+++ api/index.js
@@ -11,7 +11,9 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+
 
 // Routes
 app.use('/api', routes);
